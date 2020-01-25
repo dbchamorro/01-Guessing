41b3cb638a1023df2000001613a6945b1f1c583f
Destiny C <dbchamorro@yahoo.com>
2020-01-25 13:14:40 -0500

    .

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 01-Guessing
 Computer Number Guessing Game
+This repository will hold the file for the Computer/User Numbers game.
+The main.py file, will hold the code required for the user to run the game. 
+The code will ask for a user input and then compare it to the randomized number it chose and provide feedback. 
---
+++ main.py
@@ -0,0 +1,36 @@
+import sys, random
+
+assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
+
+r = random.randint(1,20)
+
+name = input('What is your name? ')
+print('Hello ' + name + ', guess a number between 1 and 20') 
+h = input()
+
+
+while name == true:
+    guess = 0
+    if h == true:
+        guesscont = guesscount + 1
+
+    if h > r:
+        print('You guessed too high, you have guessed {guesscount} time(s). Try again')
+    if h < r:
+        print('You guessed to low, you have guessed {guesscount} time(s). Try again.')
+    if h == r:
+        break
+if h == r:
+    guesscont = str(guesscont)
+    print('You got it! It took you'+ guesscont + 'time(s). ')
+if h = false
+    print("Uh oh, you have to guess a number between 1 and 20.")
+if h < 1:
+    print("Uh oh, you have to guess a number between 1 and 20.")
+if h > 20:
+    print("Uh oh, you have to guess a number between 1 and 20.") 
+    
+
+
+
+
